74dbde5ad21932e67b60ab16af3e5000dc39b6db
Andrew Campbell <campbell.drew.m@gmail.com>
2017-06-19 06:45:29 -0400

    Pass tests for 02_Calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(num1, num2)
+  result = num1 + num2
+end
+
+def subtract(num1, num2)
+  result = num1 - num2
+end
+
+def sum(array)
+  result = 0.0
+  if array.length == 0 then
+    result = 0
+  else
+    array.each do |num|
+      result += num
+    end
+  end
+  return result
+end
