216e3cd1780785606ccf531fa271e00df42497a5
Andrew Campbell <campbell.drew.m@gmail.com>
2017-06-23 13:48:52 -0400

    complete pig latin translate

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+def translate(sentence)
+  wordarr = sentence.split()
+  vowels = ["a","e","i","o","u"]
+  wordarr.map do |word|
+    word.downcase!
+    if vowels.include?(word[0])
+      word<<"ay"
+    elsif
+      uvwl = word.index('u')
+      word = word << word[0..uvwl] << "ay"
+      word.slice!(0..uvwl)
+    else
+      # word = word<<word[0]<<"ay"
+      # word.slice!(0)
+      consts = word.index(/[aeiou]/)-1
+      word = word << word[0..consts] << "ay"
+      word.slice!(0..consts)
+    end
+  end
+  wordarr.join(" ")
+end
+
+
+puts translate("apple")
+puts translate("string")
+puts translate("quiet")
