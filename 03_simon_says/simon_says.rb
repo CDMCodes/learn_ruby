72fad215d1c3d4a7d192b16c5803e17d4a0b02ab
Andrew Campbell <campbell.drew.m@gmail.com>
2017-06-23 06:45:21 -0400

    completes part 3 after much frustration

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,59 @@
 #write your code here
+def echo(words)
+  words
+end
+
+def shout(words)
+  words.upcase
+end
+
+def repeat(words, num=2)
+  result = words
+  num = num-1
+  num.times do
+    result += " #{words}"
+  end
+  result
+end
+
+def start_of_word(word,num=1)
+  word[0..num-1]
+end
+
+def first_word(words)
+  words.split(' ')[0]
+end
+
+# def titleize(words)
+#   skipwords = ["over", "the", "and"]
+#   wordarr = words.split
+#   #puts wordarr
+#   #wordarr.join(" ")
+#   arr2 = wordarr.map do |w|
+#     if skipwords.include?(w)
+#       w
+#     else
+#       w.capitalize
+#     end
+#   end
+#     arr2[0].capitalize!
+#     arr2[-1].capitalize!
+#     arr2.join(" ")
+#     arr2
+#     puts arr2
+# end
+
+def titleize(s)
+  words = s.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
+
+puts titleize("jaws")=="Jaws"
+#titleize("Firstname lastname")
