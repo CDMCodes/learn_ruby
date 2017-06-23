e20452cddf4962b1499bc8f23e22ec2589dc50ef
Andrew Campbell <campbell.drew.m@gmail.com>
2017-06-23 14:56:49 -0400

    completes book class and methods

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,37 @@
 class Book
 # write your code here
+
+  def title=(new_title)
+    if new_title == nil
+      @title = nil
+    else
+      words = new_title.split
+      # puts words
+      articles = ["the","and","a","an","of","in","on"]
+      words = words.map do |word|
+        if articles.include?(word)
+          word = word.downcase
+        elsif word == "i"
+          word.capitalize!
+        else
+          word = word.capitalize
+        end
+      end
+      # puts words[0]
+      words[0] = words[0].capitalize
+      # @title = new_title.capitalize!
+      @title = words.join(" ")
+    end
+  end
+
+  # attr_accessor :title
+  attr_reader :title
+
 end
+
+# shorttest = Book.new
+# shorttest.title = "bookname"
+# puts shorttest.title
+longtest = Book.new
+longtest.title = "a long title"
+puts longtest.title
